4e629f1d3c08b4a5e2846091c99076d6cc724110
ShivcharanS <sshivcharan2008@gmail.com>
2021-10-21 18:29:58 +0530

    commit

---
+++ config.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyC4EkLjPfQgZpYluayOBWX3ZN5LeLJgdFg",
+    authDomain: "spectagram-c58f8.firebaseapp.com",
+    projectId: "spectagram-c58f8",
+    storageBucket: "spectagram-c58f8.appspot.com",
+    messagingSenderId: "434368270621",
+    appId: "1:434368270621:web:71ec4053d9fb0dab5e51f4"
+  };
